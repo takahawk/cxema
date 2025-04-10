6c60405c5dd65ae6543ca260f54febe2a6a4ac17
0 <ghostoftheshell@mailbox.org>
2025-04-10 17:28:56 +0300

    Create tokenizer

---
+++ tokenizer.c
@@ -0,0 +1,35 @@
+#include "tokenizer.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+static char* next(Tokenizer *self) 
+{
+	// TODO: impl
+	return NULL;
+}
+
+static Tokenizer* from_string(char *str) {
+	Tokenizer *tokenizer = malloc(sizeof(*tokenizer));
+	size_t len = strlen(str);
+	tokenizer->str = malloc(len + 1);
+	tokenizer->i = 0;
+
+	return tokenizer;
+}
+
+static void release(Tokenizer **pself)
+{
+	Tokenizer *self = *pself;
+	free(self->str);
+	free(*pself);
+	*pself = NULL;
+}
+
+const struct _TokenizerStatic TOKENIZER = {
+	.prototype = {
+		.next = next,
+		.release = release
+	},
+	.from_string = from_string
+};
---
+++ tokenizer.h
@@ -0,0 +1,23 @@
+#ifndef TOKENIZER_H_
+#define TOKENIZER_H_
+#include <string.h>
+#include <stddef.h>
+
+typedef struct Tokenizer Tokenizer;
+
+struct Tokenizer {
+	char *str;
+	size_t i;
+
+	char* (*next)    (Tokenizer *self);
+	void  (*release) (Tokenizer **pself);
+};
+
+struct _TokenizerStatic {
+	Tokenizer prototype;
+	Tokenizer* (*from_string) (char *str);
+};
+
+extern const struct _TokenizerStatic TOKENIZER;
+
+#endif

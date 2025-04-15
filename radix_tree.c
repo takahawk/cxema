9c5402b8a114aa4ef24736456adc9c1080e8d33e
0 <ghostoftheshell@mailbox.org>
2025-04-15 04:07:48 +0300

    Add radix tree interface

---
+++ radix_tree.c
@@ -0,0 +1,26 @@
+#include "radix_tree.h"
+
+static void add(RadixTree *self, char *key, void *data, size_t size)
+{
+  // TODO: impl
+}
+
+static void* get(RadixTree *self, char *key)
+{
+  // TODO: impl
+}
+
+static void remove(RadixTree *self, char *key)
+{
+  // TODO: impl
+}
+
+static void release(RadixTree **pself)
+{
+  // TODO: impl
+}
+
+static RadixTree* form(void)
+{
+  // TODO: impl
+}
